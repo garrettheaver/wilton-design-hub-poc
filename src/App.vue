74959f58b1569d35eb0c370c311dcc0fd105749c
Garrett Heaver <garrett@hjkl.ie>
2022-06-17 17:39:58 +0100

    added a basic carpet texture option

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,11 @@
       </div>
       <div class="dropper">
         <label for="useHalfDrop">Use "Half Drop" Tiling</label>
-        <input type="checkbox" id="useHalfDrop" v-model="useHalfDrop" @change="onDropChange">
+        <input type="checkbox" id="useHalfDrop" v-model="useHalfDrop" @change="onOptChange">
+      </div>
+      <div class="texture">
+        <label for="useTexture">Use Carpet "Texture" Mapping</label>
+        <input type="checkbox" id="useTexture" v-model="useTexture" @change="onOptChange">
       </div>
     </div>
     <div class="canvas">
@@ -47,8 +51,10 @@ export default {
   data() {
     return {
       name: '',
+      useTexture: false,
       useHalfDrop: false,
       image: '',
+      texture: '',
       palette: [],
       images: [
         'AC15918',
@@ -63,20 +69,22 @@ export default {
         '28617-300',
         '28618-88',
         '28619-77',
-        '28620-76'
+        '28620-76',
+        '69432-14'
       ]
     }
   },
   methods: {
     async onSelectImage(event) {
-      this.useHalfDrop = false;
+      this.useTexture = false
+      this.useHalfDrop = false
       await this.setImage(event.target.value)
     },
-    async onDropChange(event) {
+    async onOptChange(event) {
       await this.setPalette(this.palette)
     },
     async setImage(name) {
-      this.name = name;
+      this.name = name
       this.image = ''
       this.palette = []
       
@@ -99,41 +107,64 @@ export default {
         palette: this.palette
       })
       .then(png => {
-        const singleCanvas = this.$refs.single;
-        singleCanvas.width = png.width;
-        singleCanvas.height = png.height;
+        const singleCanvas = this.$refs.single
+        singleCanvas.width = png.width
+        singleCanvas.height = png.height
 
-        const tempContext = singleCanvas.getContext('2d');
-        tempContext.putImageData(png, 0, 0);
+        const tempContext = singleCanvas.getContext('2d')
+        tempContext.putImageData(png, 0, 0)
 
-        const dropCanvas = this.$refs.drop;
-        dropCanvas.width = png.width * 2;
-        dropCanvas.height = png.height;
+        const dropCanvas = this.$refs.drop
+        dropCanvas.width = png.width * 2
+        dropCanvas.height = png.height
         
-        console.log(this.drop)
-        const dropContext = dropCanvas.getContext('2d');
-        dropContext.putImageData(png, 0, 0);
+        const dropContext = dropCanvas.getContext('2d')
+        dropContext.putImageData(png, 0, 0)
 
-        const dropHeight = this.useHalfDrop ? png.height / 2 : 0;
-        dropContext.putImageData(png, png.width, -dropHeight);
-        dropContext.putImageData(png, png.width, dropHeight);
+        const dropHeight = this.useHalfDrop ? png.height / 2 : 0
+        dropContext.putImageData(png, png.width, -dropHeight)
+        dropContext.putImageData(png, png.width, dropHeight)
 
-        const img = new Image();
-        img.src = dropCanvas.toDataURL('image/png');
+        const tiledCanvas = this.$refs.tiled
+        const threeDView = this.$refs.threeDView
 
-        const tiledCanvas = this.$refs.tiled;
-        const threeDView = this.$refs.threeDView;
+        tiledCanvas.height = window.innerHeight
+        tiledCanvas.width = tiledCanvas.parentElement.clientWidth - 5
 
-        tiledCanvas.height = window.innerHeight;
-        tiledCanvas.width = tiledCanvas.parentElement.clientWidth - 5;
+        const useTexture = this.useTexture
+        const texture = this.texture
 
+        const img = new Image()
+        img.src = dropCanvas.toDataURL('image/png')
+        
         img.onload = function() {
           const context = tiledCanvas.getContext('2d')
           context.fillStyle = context.createPattern(img, 'repeat')
-          context.fillRect(0, 0, tiledCanvas.width, tiledCanvas.height);
-          threeDView.style = `background-image: url(${tiledCanvas.toDataURL('image/png')})`;
+          context.fillRect(0, 0, tiledCanvas.width, tiledCanvas.height)
+
+          if (useTexture) {
+            context.filter = "brightness(1.5)"
+            context.globalCompositeOperation = 'multiply'
+            context.fillStyle = context.createPattern(texture, 'repeat')
+            context.fillRect(0, 0, tiledCanvas.width, tiledCanvas.height)
+          }
+
+          threeDView.style = `background-image: url(${tiledCanvas.toDataURL('image/png')})`
         }
-      });
+      })
+    },
+    async loadTexture() {
+      if (this.texture !== '') {
+        return Promise.resolve()
+      }
+
+      return new Promise((resolve, reject) => {
+          this.texture = new Image()
+          this.texture.src = "assets/texture.png"
+          this.texture.onload = function () {
+            resolve()
+          }
+      })
     },
     onColorChange(newColor) {
       const newPalette = Uint8Array.from(this.palette)
@@ -142,20 +173,21 @@ export default {
       newPalette[colorOffset + 1] = newColor.color[1]
       newPalette[colorOffset + 2] = newColor.color[2]
       newPalette[colorOffset + 3] = newColor.color[3]
-      this.setPalette(newPalette);
+      this.setPalette(newPalette)
     }
   },
   computed: {
     colors() {
-      const chunks = [];
+      const chunks = []
       const palette = this.palette || []
       for (let i = 0; i < palette.length; i += 4) {
           chunks.push(palette.slice(i, i + 4))
       }
-      return chunks;
+      return chunks
     }
   },
   async mounted() {
+    await this.loadTexture()
     await this.setImage('28616-89')
   }
 }
